386156ace1439820b506fe8174e37031b4a0c2c5
1egoman <rsg1egoman@gmail.com>
2015-03-20 08:19:16 -0400

    formatted wolfram definitions

--- schemes/nlp/wolfram.js
+++ schemes/nlp/wolfram.js
@@ -1,13 +1,39 @@
 var lou = require("../lou");
 var wa = require("wolfram-alpha");
 var _ = require("underscore");
+var natural = require("natural");
 
 module.exports = function(raw, prefs, callback) {
   // callback(null, data) for success
   // callback(true, null) if data isn't relevant
 
-  var formatOutput = function(raw) {
+  var formatOutput = function(raw, input) {
     str = raw.trim();
+
+    if ( str.indexOf("nown") !== -1 || str.indexOf("adjective") !== -1 || str.indexOf("verb") !== -1) {
+      // it's a definition
+      str = _.map(str.split("\n"), function(row) {
+        parts = row.split("|");
+
+        // format into sentences.
+        // do we have mutiple definitions?
+        if ( parseInt(parts[0]) ) {
+          parts[0] = natural.CountInflector.nth(parseInt(parts[0]));
+
+          // put a word before the definition to introduce it better
+          preword = "";
+          if (parts[1].trim() === "verb") preword = "to ";
+
+          // the definition
+          return parts[0] + ", a " + parts[1].trim() + ", " + preword + parts[2];
+        } else {
+          return "a " + parts[0] + " - " + parts[1];
+        };
+
+      });
+      str = str.join("; ");
+
+    }
 
     if (str.indexOf('|') !== -1) {
 
@@ -35,7 +61,7 @@ module.exports = function(raw, prefs, callback) {
         phrase = primary_pod[0].subpods.map(function(i) { return i.text; }).join(' ');
         callback(err, {
           response: {
-            text: formatOutput(phrase)
+            text: formatOutput(phrase, raw)
           },
           datapoints: {
             by: "nlp.wolfram"
